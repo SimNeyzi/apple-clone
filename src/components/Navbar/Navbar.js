65f0164b8597d7c551eccdc0500866b376cd97a2
Sim Neyzi <simneyzi@gmail.com>
2023-03-27 15:48:24 -0700

    add nav bar animation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { FaApple } from "react-icons/fa";
 import { TfiSearch } from "react-icons/tfi";
@@ -19,42 +19,55 @@ const pages = [
 ];
 
 const Navbar = () => {
+  const [isShown, setIsShown] = useState(false);
   return (
-    <nav className="nav">
-      <ul className="pages">
-        <li className="logo">
-          <a href="/" className="logo__link">
-            <FaApple />
-          </a>
-        </li>
-        {pages.map((page, key) => (
-          <li className="page" key={key}>
-            <a href="/" className="page__link">
-              {page}
+    <>
+      <nav className="nav">
+        <ul className="pages">
+          <li className="logo">
+            <a href="/" className="logo__link">
+              <FaApple />
             </a>
           </li>
-        ))}
-      </ul>
+          {pages.map((page, key) => (
+            <li
+              className="page"
+              key={page + key}
+              onMouseEnter={() => setIsShown(true)}
+              onMouseLeave={() => setIsShown(false)}
+            >
+              <a href="/" className="page__link">
+                {page}
+              </a>
+            </li>
+          ))}
+        </ul>
 
-      <ul className="icons">
-        <li className="search">
-          <a href="/" className="search__link">
-            <TfiSearch />
-          </a>
-        </li>
-        <li className="bag">
-          <a href="/" className="bag__link">
-            <BsBag />
-          </a>
-        </li>
+        <ul className="icons">
+          <li className="search">
+            <a href="/" className="search__link">
+              <TfiSearch />
+            </a>
+          </li>
+          <li className="bag">
+            <a href="/" className="bag__link">
+              <BsBag />
+            </a>
+          </li>
 
-        <li className="menu">
-          <a href="/" className="menu__link">
-            <TbMenu />
-          </a>
-        </li>
-      </ul>
-    </nav>
+          <li className="menu">
+            <a href="/" className="menu__link">
+              <TbMenu />
+            </a>
+          </li>
+        </ul>
+      </nav>
+      {isShown && (
+        <div className="nav-flyout-scrool-container">
+          <div className="nav-flyout-content">Shop</div>
+        </div>
+      )}
+    </>
   );
 };
 
